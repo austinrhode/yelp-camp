7237819e494148fc7a39956195ff62893db58e36
ajr3021 <ajr3021@g.rit.edu>
2021-01-20 13:10:34 -0500

    Updated model to reflect server side validations

--- models/campground.js
+++ models/campground.js
@@ -2,11 +2,27 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const campgroundSchema = new Schema({
-    title: String,
-    image: String,
-    description: String,
-    price: Number,
-    location: String  // will switch to coordinates in the future
+    title: {
+        type: String,
+        required: true
+    },
+    image:  {
+        type: String,
+        required: true
+    },
+    description:  {
+        type: String,
+        required: true
+    },
+    price:  {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    location: {
+        type: String,
+        required: true
+    }, // will switch to coordinates in the future
 })
 
 module.exports = new mongoose.model("Campground", campgroundSchema);
