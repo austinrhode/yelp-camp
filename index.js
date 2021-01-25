fb8679e494e373a934f59aa5a05c6e4d89d57181
ajr3021 <ajr3021@g.rit.edu>
2021-01-25 14:54:23 -0500

    Adjusted form validation to ber served as a static asset

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.engine('ejs', ejsMate);
 
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log("LISTENING ON PORT", port);
